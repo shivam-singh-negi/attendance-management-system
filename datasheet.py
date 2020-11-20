f6a3ab9d293bc3916be51fa5b30970f28c7d8499
shivam-singh-negi <56045882+shivam-singh-negi@users.noreply.github.com>
2020-11-20 16:04:57 +0530

    Delete datasheet.py

--- datasheet.py
+++
@@ -1,7 +0,0 @@
-import csv
-from datetime import datetime
-
-with open("data collected\\attendance list.csv", 'a+', newline='') as csvfile:
-    field_names = ['Date', 'Name', 'Entry Time', 'Exit Time', 'Duration', 'Status']
-    writer = csv.DictWriter(csvfile, fieldnames=field_names)
-    writer.writeheader()
